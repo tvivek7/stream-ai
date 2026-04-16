60aed5a7dfff96f55e8a07162a0cbaa06629e2c7
Vivek Tiwari <vivekt123456@gmail.com>
2026-04-16 19:28:20 +0530

    new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <>
-      <div className="text-3xl font-bold">Let's Cook</div>
-    </>
-  );
+  return <Body />;
 }
 
 export default App;
